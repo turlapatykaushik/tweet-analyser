d29ef2a3b81a4ba0cd908a282654b78a54f990ce
turlapatykaushik <turlapatykaushik95@gmail.com>
2015-07-27 18:11:21 +0530

    Added

---
+++ data_analysis.py
@@ -0,0 +1,44 @@
+import json,re
+import pandas as pd
+import matplotlib.pyplot as plt
+
+tweets_data_path = 'Give the PATH to the file in which text is stored'
+
+tweets_data = []
+tweets_file = open(tweets_data_path, "r")
+for line in tweets_file:
+    try:
+        tweet = json.loads(line)
+        tweets_data.append(tweet)
+    except:
+        continue
+print len(tweets_data)
+tweets = pd.DataFrame()
+tweets['text'] = map(lambda tweet: tweet['text'], tweets_data)
+
+def word_in_text(word, text):
+    word = word.lower()
+    text = text.lower()
+    match = re.search(word, text)
+    if match:
+        return True
+    return False
+tweets['johncena'] = tweets['text'].apply(lambda tweet: word_in_text('johncena', tweet))
+tweets['undertaker'] = tweets['text'].apply(lambda tweet: word_in_text('undertaker', tweet))
+tweets['randyorton'] = tweets['text'].apply(lambda tweet: word_in_text('randyorton', tweet))
+print tweets['johncena'].value_counts()[True]
+print tweets['undertaker'].value_counts()[True]
+print tweets['randyorton'].value_counts()[True]
+wrestlers = ['johncena','undertaker', 'randyorton']
+tweets_by_wrestlers = [tweets['johncena'].value_counts()[True], tweets['undertaker'].value_counts()[True], tweets['randyorton'].value_counts()[True]]
+
+x_pos = list(range(len(wrestlers)))
+width = 0.8
+fig, ax = plt.subplots()
+plt.bar(x_pos, tweets_by_wrestlers, width, alpha=1, color='g')
+
+ax.set_ylabel('Number of tweets', fontsize=15)
+ax.set_title('Ranking: johncena vs. undertaker vs. randyorton (Raw data)', fontsize=10, fontweight='bold')
+ax.set_xticks([p + 0.4 * width for p in x_pos])
+ax.set_xticklabels(wrestlers)
+plt.grid()
---
+++ data_receiver.py
@@ -0,0 +1,33 @@
+#Import the necessary methods from tweepy library
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+
+#Variables that contains the user credentials to access Twitter API 
+access_token = "Place Your twitter api access token"
+access_token_secret = "Place your twitter api access token secret"
+consumer_key = "Place your twitter api consumer key"
+consumer_secret = "Place your twitter api consumer secret key"
+
+
+#This is a basic listener that just prints received tweets to stdout.
+class StdOutListener(StreamListener):
+
+    def on_data(self, data):
+        print data
+        return True
+
+    def on_error(self, status):
+        print status
+
+
+if __name__ == '__main__':
+
+    #This handles Twitter authetification and the connection to Twitter Streaming API
+    l = StdOutListener()
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    stream = Stream(auth, l)
+
+    #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
+    stream.filter(track=['johncena', 'undertaker', 'randyorton'])
