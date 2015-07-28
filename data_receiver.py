8bc80959503bebb3978cd3e8f84eb72e421d52f7
turlapatykaushik <turlapatykaushik95@gmail.com>
2015-07-28 21:27:46 +0530

    Updated

--- data_receiver.py
+++ data_receiver.py
@@ -28,5 +28,5 @@ if __name__ == '__main__':
     auth.set_access_token(access_token, access_token_secret)
     stream = Stream(auth, l)
 
-    #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
+    #This line filter Twitter Streams to capture data by the keywords: 'johncena', 'undertaker', 'randyorton'
     stream.filter(track=['johncena', 'undertaker', 'randyorton'])
